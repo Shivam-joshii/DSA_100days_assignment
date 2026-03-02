b50840d7218c62a7176f46d9de70c62dc82604d3
Shivam-joshii <shivam.jooshii@gmail.com>
2026-03-02 12:11:36 +0530

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+};
+
+struct Node* createNode(int c, int e) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->coeff = c;
+    newNode->exp = e;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n, c, e;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *prev = NULL;
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d %d", &c, &e);
+        temp = createNode(c, e);
+
+        if(head == NULL)
+            head = temp;
+        else
+            prev->next = temp;
+
+        prev = temp;
+    }
+
+    temp = head;
+    while(temp != NULL) {
+        if(temp->exp == 0)
+            printf("%d", temp->coeff);
+        else if(temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%dx^%d", temp->coeff, temp->exp);
+
+        if(temp->next != NULL)
+            printf(" + ");
+
+        temp = temp->next;
+    }
+
+    return 0;
+}
